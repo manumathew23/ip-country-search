7ee2ea9a098006835c39c80f960e19c6ed6870a7
Manu Mathew <manu_mathew@outlook.com>
2019-06-24 09:00:32 +0530

    Initial commit

---
+++ helpers.py
@@ -0,0 +1,60 @@
+import requests
+import csv
+import math
+from ipaddress import ip_address, IPv4Address
+import bisect
+
+from constants import *
+
+def download_data(rir_entry):
+    rir = rir_entry[0]
+    link = rir_entry[1]
+    data = requests.get(link)
+    if data.status_code == 200:
+        file_name = rir + ".csv"
+        with open(file_name, mode='wb') as file:
+            file.write(data.content)
+
+def size_to_cidr_mask(c):
+    """ c = 2^(32-m), m being the CIDR mask """
+    return int(-math.log2(c) + 32)
+
+def parse_rir_file(filename):
+    with open(filename) as f:
+        rows = csv.reader(f, delimiter='|')
+        for r in rows:
+            try:
+                rir, country_code, ip_version, ip, mask, *_ = r
+            except ValueError:
+                continue
+            if ip == '*':
+                continue
+            if ip_version == 'ipv4':
+                length = int(mask)
+                addr = ip_address(ip)
+                yield {
+                    'ip_low': addr,
+                    'ip_high': addr + length - 1,
+                    'rir': rir,
+                    'country': country_code,
+                    'range': ip+'/'+str(size_to_cidr_mask(length)),
+                }
+
+def get_ip_data(keys, data, ip):
+    ip = ip_address(ip)
+    if not ip.is_global or ip.is_multicast:
+        return None
+    i = bisect.bisect_right(keys, ip)
+    entry = data[i-1]
+    assert(entry['ip_low'] <= ip <= entry['ip_high'])
+    return entry
+
+def validate_ip(ip):
+    try:
+        ip_address(ip)
+        return True
+    except ValueError:
+        return False
+
+def parse_ip_data(ip_data):
+    return COUNTRY_MAPPING.get(ip_data.get('country'), "Unknown country code")
---
+++ ip_country_search/main.py
@@ -0,0 +1,41 @@
+import concurrent.futures
+import csv
+import math
+from ipaddress import ip_address, IPv4Address
+import bisect
+from urllib.parse import urlencode, quote_plus
+import itertools
+
+from constants import *
+from helpers import *
+
+
+def update_rir_files():
+    with concurrent.futures.ProcessPoolExecutor(max_workers=len(RIR_URL_MAPPING)) as executor:
+        executor.map(download_data, RIR_URL_MAPPING.items())
+
+
+rir_file = [rir + ".csv" for rir in list(RIR_URL_MAPPING.keys())]
+
+# Parse RIR file data
+data = list(itertools.chain(
+    parse_rir_file(rir_file[0]),
+    parse_rir_file(rir_file[1]),
+    parse_rir_file(rir_file[2]),
+    parse_rir_file(rir_file[3]),
+    parse_rir_file(rir_file[4])
+))
+
+data.sort(key=lambda r: r['ip_low'])
+keys = [r['ip_low'] for r in data]
+
+def get_country_name(ip_address=None):
+    if not ip_address:
+        return {"status_code": 400, "message": "empty IP"}
+    print('bb')
+    if validate_ip(ip_address):
+        return {
+            "status_code": 200,
+            "message": parse_ip_data(get_ip_data(keys, data, ip_address))
+        }
+    return {"status_code": 400, "message": "Incorrect IP"}
